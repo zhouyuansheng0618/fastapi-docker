01dbe032ef82493ff0dfc54f673ff472b52f4f86
zhouys618 <1767703660@qq.com>
2022-06-15 14:03:59 +0800

    1

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ email： zhouys618@163.com
 datetime： 2022/6/14 22:42 
 ide： PyCharm
 """
+import uvicorn
 from typing import Optional
 
 from fastapi import FastAPI
@@ -21,3 +22,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app=app,host='0.0.0.0',port=80)
